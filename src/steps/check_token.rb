d50163b36cf20f578fe0a8bd6e9df28e0300c12f
Miro Zurbuchen <zurbuchen@puzzle.ch>
2018-08-23 10:13:12 +0200

    check token, refs #43750

--- src/steps/check_token.rb
+++ src/steps/check_token.rb
@@ -1,4 +1,30 @@
 # frozen_string_literal: true
 
-class Steps::CheckToken
+require_relative 'step'
+
+class CheckToken < Step
+  def initialize(token)
+    super()
+    @sent_token = token
+  end
+
+  def execute
+    if valid?
+      @state = 200
+      return true
+    end
+    @state = 401
+    errors << t('error_messages.authentication_failed')
+    false
+  end
+
+  private
+
+  def valid?
+    token == @sent_token
+  end
+
+  def token
+    ENV['MAIL_MON_TOKEN']
+  end
 end
