f659643974051925635aaf7a47760e4136bb18d9
Miro Zurbuchen <zurbuchen@puzzle.ch>
2018-08-23 09:39:46 +0200

    mailbox model

--- src/models/mailbox.rb
+++ src/models/mailbox.rb
@@ -1,3 +1,26 @@
 # frozen_string_literal: true
 
-dfd
+require_relative '../lib/locales_helper'
+
+class Mailbox
+  attr_reader :name, :folders, :status, :errors, :description, :imap_config
+  attr_writer :status
+
+  def initialize(name, description = '', folders, imap_config)
+    @name = name
+    @description = description
+    @folders = folders
+    @imap_config = imap_config
+    @status = nil
+    @errors = []
+
+    validate
+  end
+
+  def validate
+    errors << t('error_messages.folders_not_valid',
+                mailbox: @name) if folders.empty?
+    errors << t('error_messages.mailbox_not_defined_in_secret_file',
+                mailbox: @name) unless imap_config
+  end
+end
---
+++ src/tests/models/mailbox_test.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+require_relative '../../models/folder'
+require_relative '../../models/mailbox'
+
+class MailboxTest < Test::Unit::TestCase
+  context 'validate mailbox' do
+    def test_mailbox_valid
+      imap_config = ImapConfig.new(mailboxname: 'mailboxname',
+                                   username: 'user',
+                                   password: 'password',
+                                   hostname: 'example.hostname.com')
+      folders = [Folder.new('folder1',
+                            'this is a folder description',
+                            2,
+                            '/(Error|Failure)/')]
+      mailbox = Mailbox.new('mailbox1',
+                            'this is a mailbox description',
+                            folders,
+                            imap_config)
+      assert_equal [], mailbox.errors
+    end
+
+    def test_mailbox_invalid_if_folders_empty
+      imap_config = ImapConfig.new(mailboxname: 'mailboxname',
+                                   username: 'user',
+                                   password: 'password',
+                                   hostname: 'example.hostname.com')
+      mailbox = Mailbox.new('mailbox1',
+                            'this is a mailbox description',
+                            [],
+                            imap_config)
+      assert_equal 'Folders in mailbox mailbox1 are not valid', mailbox.errors
+                                                                       .first
+    end
+  end
+end
