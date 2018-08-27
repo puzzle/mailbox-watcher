51eb6691691d8de5910f8b431aa52e0139baaf26
Miro Zurbuchen <zurbuchen@puzzle.ch>
2018-08-27 12:18:25 +0200

    fixed collapse bug

--- src/public/javascripts/collapse.js
+++ src/public/javascripts/collapse.js
@@ -1,3 +1,17 @@
-function changeIcon(e) {
-  $(e.target).toggleClass('glyphicon-chevron-down glyphicon-chevron-left');
+$(document).ready(function() {
+  $('.collapse').on('show.bs.collapse', function(e) {
+    changeIcon(e, 'glyphicon-chevron-left', 'glyphicon-chevron-down');
+  });
+  
+  $('.collapse').on('hide.bs.collapse', function(e) {
+    changeIcon(e, 'glyphicon-chevron-down', 'glyphicon-chevron-left');
+  });
+});
+
+function changeIcon(e, class1, class2) {
+  id = $(e.target).attr('id');
+  collapseIcon = $('#icon-' + id);
+
+  collapseIcon.removeClass(class1);
+  collapseIcon.addClass(class2);
 }
