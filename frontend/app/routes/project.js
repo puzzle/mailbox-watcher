9c682ffcbee8f355f056ac2150f1f796f49eda98
Nils Rauch <rauch@puzzle.ch>
2018-10-30 13:36:52 +0100

    added project not found message

--- frontend/app/routes/project.js
+++ frontend/app/routes/project.js
@@ -4,6 +4,16 @@ export default Route.extend({
   model(params) {
     var mailMonToken = localStorage.getItem('mail_mon_token')
     return this.store.findRecord('project', params.projectname, { token: mailMonToken })
+      .then(function(project){
+        return project
+      }).catch(function(error){
+        if (error.errors[0].status == 404) {
+          return {
+                  'error': true,
+                  'message': "Project '" + params.projectname + "' was not found"
+                  };
+        }
+      });
   },
 
   actions: {
