583ed98ef57d215d015c6588bcd4659aec748fce
Nils Rauch <rauch@puzzle.ch>
2018-10-30 14:50:06 +0100

    fixed test

--- frontend/tests/integration/helpers/token-test.js
+++ frontend/tests/integration/helpers/token-test.js
@@ -1,12 +1,12 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('token', 'helper:token', {
+moduleForComponent('authenticityToken', 'helper:token', {
   integration: true
 });
 
 test('it works', function(assert) {
-  this.render(hbs`{{token}}`);
+  this.render(hbs`{{authenticityToken}}`);
 
   assert.equal(this.$().text().trim(), 'false');
 });
