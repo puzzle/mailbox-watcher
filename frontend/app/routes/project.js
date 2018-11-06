33a71c7e2bfeb2fcf931f4f4218699dbeb63947e
Pascal Simon <simon@puzzle.ch>
2018-11-06 23:45:11 +0100

    fix loading model in project route

--- frontend/app/routes/project.js
+++ frontend/app/routes/project.js
@@ -1,16 +1,8 @@
 import AuthenticatedRoute from "./authenticated_route";
-import { inject as service } from "@ember/service";
 
 export default AuthenticatedRoute.extend({
-  pendingOperation: service(),
   model(params) {
-    pendingOperation.start();
-    this.store
-      .findRecord("project", params.projectname)
-      .then(function(project) {
-        pendingOperation.stop();
-        return project;
-      });
+    return this.store.findRecord("project", params.projectname);
   },
 
   actions: {
